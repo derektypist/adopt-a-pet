28dffca39d4575f540ba20700decfdf0ddc4918d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-03-02 09:50:42 +0000

    Added Route and Switch at App.js

--- adoptapet/src/App.js
+++ adoptapet/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
