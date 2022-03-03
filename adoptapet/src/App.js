c14c30f4a9960844e37e70be9805fd3b351509a2
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-03-03 10:16:08 +0000

    Added Navigation at App.js

--- adoptapet/src/App.js
+++ adoptapet/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/navigation'
 import { BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Route path="/:type">
-
+        <Navigation />
       </Route>
 
       <Route path="/:type?">
