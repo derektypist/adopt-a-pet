8df8e9da4a71930cf4df4a704fbb92ae851c8477
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-03-03 09:57:57 +0000

    Added useParams at index.js

--- adoptapet/src/pages/home/index.js
+++ adoptapet/src/pages/home/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { getPets } from '../../api/petfinder';
 import Hero from '../../components/hero';
 import Pet from '../../components/pet';
+import {useParams} from 'react-router-dom'
 
 const HomePage = () => {
   const [data, setData] = useState(null);
