c9cffaa8e66f54e0361db41c6554fcea60e58bef
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-03-10 10:10:27 +0000

    Updated App.js - imports

--- adoptapet/src/App.js
+++ adoptapet/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './components/navigation'
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
 import { BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 function App() {
